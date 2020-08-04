2e9fd1d9a1655b961bc63faf7ef010bb7ac3d66f
nazirjon <nazirjon-1997@mail.ru>
2020-08-04 19:22:10 +0300

    demo commit2

--- app/src/main/java/com/developer/testtask/ui/ViewActivity.java
+++ app/src/main/java/com/developer/testtask/ui/ViewActivity.java
@@ -1,30 +1,12 @@
 package com.developer.testtask.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.developer.testtask.R;
-import com.developer.testtask.adapters.UsersAdapter;
-import com.developer.testtask.databases.UserRepository;
-import com.developer.testtask.local.UserDataSource;
-import com.developer.testtask.local.UserDatabase;
-import com.developer.testtask.models.User;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
 public class ViewActivity extends AppCompatActivity {
     TextView nameView, emailView;
